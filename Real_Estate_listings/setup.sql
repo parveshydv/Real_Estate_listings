4d67e43cd9a5d8e127729c3cd5e421063f13ebec
Parvesh Yadav <parveshahir@gmail.com>
2024-06-09 16:21:28 -0700

    1 revision

--- Real_Estate_listings/setup.sql
+++ Real_Estate_listings/setup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE testing;
-USE testing;
+CREATE DATABASE Real_Estate_listing;
+USE Real_Estate_listing;
 
 CREATE TABLE 'uses'(
     id VARCHAR(128),
@@ -34,15 +34,15 @@ CREATE TABLE `customer`(
     `name` VARCHAR(128),
     `email` VARCHAR(128),
     `address` VARCHAR(128),
-    `num` INT,
+    `num` VARCHAR(128),
     `id` VARCHAR(128)
 );
 
 INSERT INTO customer
-VALUES ('Ruhil','Nitin','nruhil@gmail.com','120 Douglas St',+1(250)883-0013,'C124');
+VALUES ('Ruhil','Nitin','nruhil@gmail.com','120 Douglas St',"+1(250)883-0013",'C124');
 
 
-CREATE TABLE `prop`(
+CREATE TABLE `prop`
     `prop_id` VARCHAR(128),
     `cust_id` VARCHAR(128),
     `prop_type` VARCHAR(128),
@@ -52,7 +52,7 @@ CREATE TABLE `prop`(
 );
 
 INSERT INTO prop
-VALUES ('P4942', 'C124', 'TownHouse','Rented','$10M','122 Hillside ave');
+VALUES ('P4942', 'C124', 'TownHouse','Sold','$10M','122 Hillside ave');
 
 
 CREATE TABLE `sales`(
@@ -73,7 +73,7 @@ CREATE TABLE `transactions`(
     `t_id` VARCHAR(128)
 );
 
-NSERT INTO transactions
+INSERT INTO transactions
 VALUES (10000,96000000,'TownHouse','P4942','T202494');
 
 
@@ -99,26 +99,3 @@ CREATE TABLE `requirement`(
 
 INSERT INTO requirement
 VALUES ('Parvesh Yadav','P2483','$5M - $6M','400 Yards','New York','Appartment');
-
-
---Retrieve Properties and Their Corresponding Sales Prices and Rental Details
-SELECT p.prop_id, p.prop_type, s.price AS sale_price, r.rent, r.security_deposit
-FROM prop p
-LEFT JOIN sales s ON p.prop_id = s.prop_id
-LEFT JOIN rent r ON p.prop_id = r.pro_id;
-
-
-
---  Find Customers Who Have Rented Properties and Their Rental Details
-SELECT c.name, c.surname, r.pro_id, r.rent, r.security_deposit
-FROM customer c
-JOIN rent r ON c.id = r.rented_to;
-
-
--- Retrieve the Total Number of Properties Sold by Each Agent
-
-
-
-
-
--- Find Properties That Are Currently Rented Out and Their Renters
